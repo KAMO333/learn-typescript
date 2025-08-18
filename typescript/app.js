601ce6ba0934f6ec461f9a89677884bcf297d94f
Kamogelo <mmopanemoroka@gmail.com>
2025-08-18 21:36:39 +0200

    flexable js

---
+++ typescript/app.js
@@ -0,0 +1,16 @@
+// DYNAMIC JAVASCRIPT = values can changes anytime, flexible but also problamatic
+
+// let box;
+// box = "hello";
+// box = 100;
+
+// console.log(typeof box); //number
+
+let box;
+console.log(typeof box); // undefined
+
+box = "Hello";
+console.log(typeof box); // string
+
+box = 100;
+console.log(typeof box); // number
