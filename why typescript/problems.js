bc6939bd8dd1acc84cce8c6feeb9b91437877eb2
Kamogelo <mmopanemoroka@gmail.com>
2025-08-18 22:10:11 +0200

    still spits js eben with errors

--- why typescript/problems.js
+++ why typescript/problems.js
@@ -1,19 +1,19 @@
 // JAVASCRIPT PROBLEMS
 
-function getProduct(id) {
-  return {
-    id: id,
-    name: `Awesome Gadget ${id}`,
-    price: 99.5,
-  };
-}
+// function getProduct(id) {
+//   return {
+//     id: id,
+//     name: `Awesome Gadget ${id}`,
+//     price: 99.5,
+//   };
+// }
 
 // const product = getProduct(1);
 // console.log(`The product ${product.Name} costs $${product.price}`);
 
-const showProduct = (name, price) => {
-  console.log(`The product ${name} costs $${price}.`);
-};
+// const showProduct = (name, price) => {
+//   console.log(`The product ${name} costs $${price}.`);
+// };
 
-const product = getProduct(1);
-showProduct(product.price, product.name);
+// const product = getProduct(1);
+// showProduct(product.price, product.name);
