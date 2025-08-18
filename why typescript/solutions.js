9154711260d21d4886677aac0e1e3cbf888d8069
Kamogelo <mmopanemoroka@gmail.com>
2025-08-18 22:13:48 +0200

    stpites js with errors

--- why typescript/solutions.js
+++ why typescript/solutions.js
@@ -1,9 +1,15 @@
 function getProduct(id) {
-    return {
-        id: id,
-        name: "Awesome Gadget ".concat(id),
-        price: 99.5,
-    };
+  return {
+    id: id,
+    name: "Awesome Gadget ".concat(id),
+    price: 99.5,
+  };
 }
+// const product = getProduct(1);
+// console.log(`The product ${product.Name} costs $${product.price}`);
+
+var showProduct = function (name, price) {
+  console.log("The product ".concat(name, " costs $").concat(price, "."));
+};
 var product = getProduct(1);
-console.log("The product ".concat(product.Name, " costs $").concat(product.price));
+showProduct(product.price, product.name);
--- why typescript/solutions.ts
+++ why typescript/solutions.ts
@@ -12,5 +12,12 @@ function getProduct(id): Product {
   };
 }
 
+// const product = getProduct(1);
+// console.log(`The product ${product.Name} costs $${product.price}`);
+
+const showProduct = (name: string, price: number) => {
+  console.log(`The product ${name} costs $${price}.`);
+};
+
 const product = getProduct(1);
-console.log(`The product ${product.Name} costs $${product.price}`);
+showProduct(product.price, product.name);
