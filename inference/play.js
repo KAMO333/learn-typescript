13e2cabe03f934b46dcc645cd5ff8af5705bd0c7
Kamogelo <mmopanemoroka@gmail.com>
2025-08-22 17:03:02 +0200

    inference

---
+++ inference/play.js
@@ -0,0 +1,5 @@
+// TYPE ANNOTATION
+var counter;
+// TYPE INFERENCE
+var newCounter = 0;
+console.log(typeof newCounter);
---
+++ inference/play.ts
@@ -0,0 +1,9 @@
+// TYPE ANNOTATION = Explicitly defining the type
+
+let counter: number;
+
+// TYPE INFERENCE = It guesses the type based on the value
+
+let newCounter = 0;
+
+console.log(typeof newCounter);
