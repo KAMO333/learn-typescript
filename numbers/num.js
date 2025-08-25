b40ab3139d467f895f15f37b29922931d463cae8
KAMO333 <mmopanemoroka@gmail.com>
2025-08-25 14:16:51 +0200

    all kinds of numbers

--- numbers/num.js
+++ numbers/num.js
@@ -1,6 +1,31 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-
 // floating-point-value
-var price = 15;
+var price;
+price = 15;
 console.log(price);
+// decimal-value
+var counter = 0;
+var x = 100, y = 200;
+console.log("decimal numbers......");
+console.log(counter);
+console.log(x);
+console.log(y);
+// binary-numbers
+var bin = 4;
+var anotherBin = 2;
+console.log("binary numbers......");
+console.log(bin);
+console.log(anotherBin);
+// Octal Numbers
+var oct = 8;
+var anotherOct = 16;
+console.log("Octal numbers......");
+console.log(oct);
+console.log(anotherOct);
+// hexadecimal numbers
+var hex = 0x10;
+var anotherHex = 0x20;
+console.log("Hexadecimal numbers......");
+console.log(hex);
+console.log(anotherHex);
--- numbers/num.ts
+++ numbers/num.ts
@@ -2,6 +2,41 @@ export {};
 
 // floating-point-value
 
-let price: number = 15;
+let price: number;
+price = 15;
 
 console.log(price);
+
+// decimal-value
+let counter: number = 0;
+let x: number = 100,
+  y: number = 200;
+
+console.log("decimal numbers......");
+console.log(counter);
+console.log(x);
+console.log(y);
+
+// binary-numbers
+let bin = 0b100;
+let anotherBin: number = 0b010;
+
+console.log("binary numbers......");
+console.log(bin);
+console.log(anotherBin);
+
+// Octal Numbers
+let oct = 0o10;
+let anotherOct: number = 0o20;
+
+console.log("Octal numbers......");
+console.log(oct);
+console.log(anotherOct);
+
+// hexadecimal numbers
+let hex = 0x10;
+let anotherHex: number = 0x20;
+
+console.log("Hexadecimal numbers......");
+console.log(hex);
+console.log(anotherHex);
