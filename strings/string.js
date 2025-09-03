f90e9dd045f34af9508a224c9b319f2688134da7
KAMO333 <mmopanemoroka@gmail.com>
2025-09-03 16:15:35 +0200

    backtick two

--- strings/string.js
+++ strings/string.js
@@ -2,5 +2,12 @@
 // let title: string = "Web Developer";
 // let fullName: string = `${firstName} - ${title}`;
 // console.log(fullName); // Output: John - Web Developer
-var description = "This TypeScript string can \nspan multiple \nlines\n";
-console.log(description);
+// let description = `This TypeScript string can
+// span multiple
+// lines
+// `;
+// console.log(description);
+var firstName = "John";
+var title = "Web Developer";
+var profile = "I'm ".concat(firstName, ". \nI'm a ").concat(title);
+console.log(profile);
