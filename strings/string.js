dd87e5f789725a044305a30f841d72d7f0d5a138
KAMO333 <mmopanemoroka@gmail.com>
2025-09-03 16:10:21 +0200

    concat strings

---
+++ strings/string.js
@@ -0,0 +1,4 @@
+var firstName = "John";
+var title = "Web Developer";
+var fullName = "".concat(firstName, " - ").concat(title);
+console.log(fullName); // Output: John - Web Developer
---
+++ strings/string.ts
@@ -0,0 +1,5 @@
+let firstName: string = "John";
+let title: string = "Web Developer";
+let fullName: string = `${firstName} - ${title}`;
+
+console.log(fullName); // Output: John - Web Developer
