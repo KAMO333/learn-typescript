dfd33b3b764b235f7648ed08284a981d8993e1d1
KAMO333 <mmopanemoroka@gmail.com>
2025-09-04 16:16:13 +0200

    refactor: comment out employee object initialization and enhance vacant type usage

--- objects/obj.ts
+++ objects/obj.ts
@@ -27,16 +27,26 @@
 
 // console.log(employee);
 
-let employee: {
-  firstName: string;
-  lastName: string;
-  age: number;
-  jobTitle: string;
-} = {
-  firstName: "John",
-  lastName: "Doe",
-  age: 25,
-  jobTitle: "Web Developer",
-};
+// let employee: {
+//   firstName: string;
+//   lastName: string;
+//   age: number;
+//   jobTitle: string;
+// } = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   age: 25,
+//   jobTitle: "Web Developer",
+// };
 
-console.log(employee);
+// console.log(employee);
+
+// empty type = {}
+
+// let vacant: {};
+// vacant.firstName = "John";
+
+let vacant: {} = {};
+console.log(vacant.toString());
+// vacant.firstName = "John";
+// console.log(vacant);
