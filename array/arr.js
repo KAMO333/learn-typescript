4f26142f9cf5de7005805305716b7a4c3c485c02
KAMO333 <mmopanemoroka@gmail.com>
2025-09-05 12:27:47 +0200

    insertion and deletion

--- array/arr.js
+++ array/arr.js
@@ -1,4 +1,8 @@
 var skills = [];
 console.log(skills);
-skills = ["Dev", "DevOps", "Cloud"];
+// skills = ["Dev", "DevOps", "Cloud"];
+skills[0] = "Problem Solving";
+skills[1] = "Programming";
+skills.push("Software Design");
+skills.pop();
 console.log(skills);
--- array/arr.ts
+++ array/arr.ts
@@ -1,5 +1,13 @@
 let skills: string[] = [];
 console.log(skills);
 
-skills = ["Dev", "DevOps", "Cloud"];
+// skills = ["Dev", "DevOps", "Cloud"];
+
+skills[0] = "Problem Solving";
+skills[1] = "Programming";
+
+skills.push("Software Design");
+
+skills.pop();
+
 console.log(skills);
