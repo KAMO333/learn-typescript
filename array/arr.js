760297ce78824d3f41977fd02b806b50ffc51e5c
KAMO333 <mmopanemoroka@gmail.com>
2025-09-05 12:33:16 +0200

    diff types in one array

--- array/arr.js
+++ array/arr.js
@@ -1,8 +1,14 @@
-var skills = [];
-console.log(skills);
-// skills = ["Dev", "DevOps", "Cloud"];
-skills[0] = "Problem Solving";
-skills[1] = "Programming";
-skills.push("Software Design");
-skills.pop();
-console.log(skills);
+// let skills: string[] = [];
+// console.log(skills);
+// // skills = ["Dev", "DevOps", "Cloud"];
+// skills[0] = "Problem Solving";
+// skills[1] = "Programming";
+// skills.push("Software Design");
+// skills.pop();
+// console.log(skills);
+// infers type as (string | number)[]
+var scores = ["Programming", 5, "Software Design", 4];
+// equivalent to:
+var same_array;
+scores = ["Programming", 5, "Software Design", 4];
+console.log(scores);
--- array/arr.ts
+++ array/arr.ts
@@ -1,13 +1,22 @@
-let skills: string[] = [];
-console.log(skills);
+// let skills: string[] = [];
+// console.log(skills);
 
-// skills = ["Dev", "DevOps", "Cloud"];
+// // skills = ["Dev", "DevOps", "Cloud"];
 
-skills[0] = "Problem Solving";
-skills[1] = "Programming";
+// skills[0] = "Problem Solving";
+// skills[1] = "Programming";
 
-skills.push("Software Design");
+// skills.push("Software Design");
 
-skills.pop();
+// skills.pop();
 
-console.log(skills);
+// console.log(skills);
+
+// infers type as (string | number)[]
+let scores = ["Programming", 5, "Software Design", 4];
+
+// equivalent to:
+let same_array: (string | number)[];
+scores = ["Programming", 5, "Software Design", 4];
+
+console.log(scores);
