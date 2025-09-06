34aeb3ba8aeaa348bcafaf2298d70faf9847a19e
Kamogelo <mmopanemoroka@gmail.com>
2025-09-06 18:50:18 +0200

    example

---
+++ tuple/example.js
@@ -0,0 +1,3 @@
+var skill;
+skill = ["Programming", 5];
+console.log("Skill: ".concat(skill[0], ", Level: ").concat(skill[1]));
---
+++ tuple/example.ts
@@ -0,0 +1,6 @@
+// Example of a tuple in TypeScript
+
+let skill: [string, number];
+skill = ["Programming", 5];
+
+console.log(`Skill: ${skill[0]}, Level: ${skill[1]}`);
