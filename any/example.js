ddb0f6210b751058ca6d55a0bcaef5a5e835f3d9
Kamogelo <mmopanemoroka@gmail.com>
2025-09-06 19:31:59 +0200

    x

--- any/example.js
+++ any/example.js
@@ -1,8 +1,14 @@
-var result;
-result = 1;
-console.log(result);
-result = "Hello";
-console.log(result);
-result = [1, 2, 3];
-var total = result.reduce(function (a, b) { return a + b; }, 0);
-console.log(total);
+// Example of using 'any' type in TypeScript
+// let result: any;
+// result = 1;
+// console.log(result);
+// result = "Hello";
+// console.log(result);
+// result = [1, 2, 3];
+// const total = result.reduce((a: number, b: number) => a + b, 0);
+// console.log(total);
+// json may come from a third-party API
+var json = "{\"latitude\": 10.11, \"longitude\":12.12}";
+// parse JSON to find location
+var currentLocation = JSON.parse(json);
+console.log(currentLocation.x);
--- any/example.ts
+++ any/example.ts
@@ -1,13 +1,20 @@
 // Example of using 'any' type in TypeScript
 
-let result: any;
+// let result: any;
 
-result = 1;
-console.log(result);
+// result = 1;
+// console.log(result);
 
-result = "Hello";
-console.log(result);
+// result = "Hello";
+// console.log(result);
 
-result = [1, 2, 3];
-const total = result.reduce((a: number, b: number) => a + b, 0);
-console.log(total);
+// result = [1, 2, 3];
+// const total = result.reduce((a: number, b: number) => a + b, 0);
+// console.log(total);
+
+// json may come from a third-party API
+const json = `{"latitude": 10.11, "longitude":12.12}`;
+
+// parse JSON to find location
+const currentLocation = JSON.parse(json);
+console.log(currentLocation.x);
