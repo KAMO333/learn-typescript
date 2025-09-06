bb6159924f9adad6eaba3457c7d6ae6e277436c4
Kamogelo <mmopanemoroka@gmail.com>
2025-09-06 18:55:47 +0200

    2

--- tuple/example.js
+++ tuple/example.js
@@ -1,6 +1,14 @@
 // Example of a tuple in TypeScript
 // let skill: [string, number];
-// // skill = ["Programming", 5];
+// skill = ["Programming", 5];
 // console.log(`Skill: ${skill[0]}, Level: ${skill[1]}`);
-var color = [255, 0, 0];
-console.log("RGB Color: (".concat(color[0], ", ").concat(color[1], ", ").concat(color[2], ")"));
+// Example of a tuple representing RGB color values
+// let color: [number, number, number] = [255, 0, 0];
+// console.log(`RGB Color: (${color[0]}, ${color[1]}, ${color[2]})`);
+var bgColor, headerColor;
+bgColor = [0, 255, 255, 0.5];
+headerColor = [0, 255, 255];
+console.log("Background Color: rgba(".concat(bgColor[0], ", ").concat(bgColor[1], ", ").concat(bgColor[2], ", ").concat(bgColor[3], ")"));
+console.log("Header Color: rgb(".concat(headerColor[0], ", ").concat(headerColor[1], ", ").concat(headerColor[2], ")"));
+console.log(bgColor);
+console.log(headerColor);
--- tuple/example.ts
+++ tuple/example.ts
@@ -5,6 +5,20 @@
 
 // console.log(`Skill: ${skill[0]}, Level: ${skill[1]}`);
 // Example of a tuple representing RGB color values
-let color: [number, number, number] = [255, 0, 0];
+// let color: [number, number, number] = [255, 0, 0];
 
-console.log(`RGB Color: (${color[0]}, ${color[1]}, ${color[2]})`);
+// console.log(`RGB Color: (${color[0]}, ${color[1]}, ${color[2]})`);
+
+let bgColor, headerColor: [number, number, number, number?];
+bgColor = [0, 255, 255, 0.5];
+headerColor = [0, 255, 255];
+
+console.log(
+  `Background Color: rgba(${bgColor[0]}, ${bgColor[1]}, ${bgColor[2]}, ${bgColor[3]})`
+);
+console.log(
+  `Header Color: rgb(${headerColor[0]}, ${headerColor[1]}, ${headerColor[2]})`
+);
+
+console.log(bgColor);
+console.log(headerColor);
